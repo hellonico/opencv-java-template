c1f0a67c24a1337fcb90b4c00f2fa27dfb187c2c
Nicolas Modrzyk <hellonico@gmail.com>
2020-01-31 15:56:22 +0900

    update origami

--- src/main/java/hello/HelloDnn.java
+++ src/main/java/hello/HelloDnn.java
@@ -1,4 +1,25 @@
 package hello;
 
+import origami.Origami;
+
 public class HelloDnn {
+
+    public static void main(String[] args) {
+        Origami.init();
+        doesntWork();
+        works();
+
+
+    }
+
+    public static void doesntWork() {
+        origami.Dnn.readNetFromSpec("networks.yolo:yolov2-tiny:1.0.0");
+    }
+
+    public static void works() {
+        String base ="/home/niko/.origami/yolov2-tiny-1.0.0.zip/yolov2-tiny/";
+        org.opencv.dnn.Dnn.readNetFromDarknet(base+"yolov2-tiny.cfg", base+"yolov2-tiny.weights");
+    }
+
+
 }
